2aefe5719c570a5ad9e7f6b8de07c535c10168b4
Santi Balaguer <santiago.balaguer@gmail.com>
2019-11-25 11:19:05 +0000

    Invoince Model Created

--- forte/models/invoice.js
+++ forte/models/invoice.js
@@ -3,14 +3,75 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: {
+  contractorName: {
     type: String,
-    trim: true
+    trim: true,
+    required: true
   },
   email: {
     type: String,
     lowercase: true,
-    trim: true
+    trim: true,
+    required: true
+  },
+  companyWorkedFor:{
+    type: String,
+    required: true
+  },
+  cellphone: {
+    type: String,
+    required: true
+  },
+  iban:{
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  jobDescription:{
+      type: String,
+      required: true
+  },
+  hiredByPerson: {
+      type: String
+  },
+  amountDue: {
+      type: Number,
+      required: true
+  },
+  vat:{
+    chargeVat: {
+        type: Boolean,
+        required: true
+    },
+    rate:{
+        type: Number,
+        enum: [18, 23]
+    }
+  },
+  irss:{
+    retention: {
+        type: Boolean,
+        required: true
+    },
+    rate:{
+        type: Number,
+        enum: [25, 30]
+    }
+  },
+  amountToTransfer: {
+      type: Number
+  },
+  comment: {
+      type: String
+  },
+  pdf: {
+      type: String,
+      required: true
+  },
+  status: {
+      type: String,
+      enum: ['unapproved', 'approved', 'rejected', 'paid']
   }
 });
 
