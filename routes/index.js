0f351915be84df98a2ef4b2b83ae216019d22f8e
José Maria Gonçalves Pereira <zemariagp@MacBook-Air-de-Jose.local>
2019-11-28 17:39:56 +0000

    lots of amazing styles

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const router = new Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Forte' });
+  res.render('index', {
+    title: 'Forte'
+  });
 });
 
 module.exports = router;
